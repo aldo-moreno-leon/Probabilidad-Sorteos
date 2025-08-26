25221571ed421ab717fa63dfed9edeba52058bf1
aldo-moreno-leon <aldo.moreno.leon@protonmail.com>
2025-08-26 14:31:28 -0700

    Sorteos de 4 digitos

---
+++ sorteo-4d-ln.py
@@ -0,0 +1,40 @@
+import random
+from collections import Counter
+
+
+def leer_archivo(nombre_archivo):
+    with open(nombre_archivo, 'r', encoding="utf-16") as archivo:
+        numeros = archivo.read().splitlines()
+        # Convertimos los números a enteros
+        numeros = [int(numero) for numero in numeros]
+        return numeros
+
+def generar_numeros(numeros, cantidad):
+    generados = []
+    while len(generados) < cantidad:
+        num = random.randint(0, 9999)
+        if num not in numeros and num not in generados:  # Aseguramos que no este en 'generados' tampoco
+            generados.append(num)
+    return generados
+
+def contar_numeros(numeros):
+    conteo = Counter(numeros)
+    repetidos = {num: conteo[num] for num in conteo if conteo[num] > 1}
+    return repetidos
+
+
+# Entrada de datos
+cantidad_numeros = int(input("Cantidad de números a generar: "))
+
+# Leer los números del archivo
+numeros = leer_archivo('numeros-4d-ln.txt')
+
+# Generar una cantidad de números que no estén en el archivo
+generados = generar_numeros(numeros, cantidad_numeros)
+
+# Contar los números que se repiten
+repetidos = contar_numeros(numeros)
+
+print("=================== Probabilidad para ganar premio mayor en sorteos ===================")
+print("Lista de numeros que han ganado mas de una vez: ", repetidos)
+print("Lista de numeros para elegir: ", generados)
